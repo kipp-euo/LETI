d63e91766bb36aad44e42368ba31a8b0ed20cda5
kipp-euo <albez2006@gmail.com>
2026-09-22 03:12:11 +0300

    Added machine word (u64) set calculation, fixed incorrect set calculation for arrs and lists

--- Algo/C++/lab1/main.cpp
+++ Algo/C++/lab1/main.cpp
@@ -1,8 +1,11 @@
 #include <chrono>
-#include <iostream>
-#include <cstdlib>
+#include <cstdint>
 #include <ctime>
 #include <clocale>
+#include <iostream>
+
+
+const wchar_t alphabet[] = L"абвгдежзийклмнопрстуфхцчшщъыьэюя";
 
 struct List {
 	wchar_t el;
@@ -12,116 +15,122 @@ struct List {
 	~List() { delete next; }
 };
 
-// Function to generate a random set of characters
-void set_fill_random(wchar_t (&result)[34]);
+void input_set(wchar_t (&result)[33], const wchar_t* name);
 
-// Function to get input from the user
-void input_set(wchar_t (&result)[34], const wchar_t* name);
-void print_set(const wchar_t (&set)[34]);
+void print_set(const wchar_t (&set)[33]);
+void print_set(const List* set);
+void print_set(uint64_t set);
 
-bool contains(const wchar_t (&arr)[34], wchar_t elem);
+void set_fill_random(wchar_t (&result)[33]);
+
+List* arr_to_list(const wchar_t (&arr)[33]);
+uint64_t arr_to_qword(const wchar_t (&arr)[33]);
+
+bool contains(const wchar_t (&arr)[33], wchar_t elem);
 bool contains(const List* l, wchar_t elem);
 
-void calc_set_arr(const wchar_t (&A)[34], const wchar_t (&B)[34],
-		const wchar_t (&C)[34], const wchar_t (&D)[34],
-		wchar_t (&out)[34]);
+void calc_set(const wchar_t (&A)[33], const wchar_t (&B)[33],
+		const wchar_t (&C)[33], const wchar_t (&D)[33],
+		wchar_t (&out)[33]);
+List* calc_set(const List* A, const List* B, const List* C, const List* D);
+uint64_t calc_set(uint64_t A, uint64_t B, uint64_t C, uint64_t D);
 
-// Functions to convert an array to a linked list
-List* arr_to_list(const wchar_t (&arr)[34]);
-void print_list(const List* head);
+void benchmark(const wchar_t (&A)[33], const wchar_t (&B)[33],
+		const wchar_t (&C)[33], const wchar_t (&D)[33]);
+void benchmark(const List* A, const List* B, const List* C, const List* D);
+void benchmark(uint64_t A, uint64_t B, uint64_t C, uint64_t D);
 
-// Function to calculate the resulting set using linked lists
-List* calc_set_list(const List* A, const List* B, const List* C, const List* D);
 
 int main()
 {
 	// For cyrillic letters
 	std::locale::global(std::locale(""));
 	std::wcout.imbue(std::locale(""));
-	// Random generation
+
 	srand((unsigned)time(NULL));
-	// Time benchmarking
-	/* const auto t_start = std::chrono::high_resolution_clock::now(); */
 
-	// MAIN LOGIC
-	wchar_t A[34], B[34], C[34], D[34], E[34];
-	// Example
-	/* wchar_t A[] = L"бвгде";
-	wchar_t B[] = L"авгде";
-	wchar_t C[] = L"абгде";
-	wchar_t D[] = L"абвде"; */
+	// // Example
+	// wchar_t A[] = L"бвгде";
+	// wchar_t B[] = L"авгде";
+	// wchar_t C[] = L"абгде";
+	// wchar_t D[] = L"абвде";
 
-	// Example from input
-	input_set(A, L"A");
-    input_set(B, L"B");
-	input_set(C, L"C");
-    input_set(D, L"D");
-
+	// // Example from input
+	// wchar_t A[33], B[33], C[33], D[33];
+	// input_set(A, L"A");
+	// input_set(B, L"B");
+	// input_set(C, L"C");
+	// input_set(D, L"D");
 
 	// Random generated examples
-	// wchar_t A[34], B[34], C[34], D[34];
-	// set_fill_random(A);
-	// set_fill_random(B);
-	// set_fill_random(C);
-	// set_fill_random(D);
+	wchar_t A[33], B[33], C[33], D[33];
+	set_fill_random(A);
+	set_fill_random(B);
+	set_fill_random(C);
+	set_fill_random(D);
 
 	std::wcout << "Sets:\n";
+	std::wcout << "A = ";
 	print_set(A);
+	std::wcout << "B = ";
 	print_set(B);
+	std::wcout << "C = ";
 	print_set(C);
+	std::wcout << "D = ";
 	print_set(D);
 
-	calc_set_arr(A, B, C, D, E);
+	// Arrays
+	benchmark(A, B, C, D);
 
-	std::wcout << L"Resulting set (array) E: ";
-	print_set(E);
+	// Linked lists
+	const List* lA = arr_to_list(A);
+	const List* lB = arr_to_list(B);
+	const List* lC = arr_to_list(C);
+	const List* lD = arr_to_list(D);
 
-	// Convert arrays to linked lists
-	List* LA = arr_to_list(A);
-    List* LB = arr_to_list(B);
-    List* LC = arr_to_list(C);
-    List* LD = arr_to_list(D);
+	benchmark(lA, lB, lC, lD);
 
-	// Calculate the resulting set using linked lists
-	List* LE = calc_set_list(LA, LB, LC, LD);
+	// Machine words
+	const uint64_t bA = arr_to_qword(A);
+	const uint64_t bB = arr_to_qword(B);
+	const uint64_t bC = arr_to_qword(C);
+	const uint64_t bD = arr_to_qword(D);
 
-	std::wcout << L"Resulting set (list)) E: ";
-    print_list(LE);
-	// END
-
-	/* const auto t_end = std::chrono::high_resolution_clock::now();
-	const double time_elapsed = std::chrono::duration<double, std::milli>(
-			t_end - t_start).count();
-	std::wcout << "Time elapsed: " << time_elapsed << '\n';  */
+	benchmark(bA, bB, bC, bD);
 }
 
-// Functions to input or print sets
-void input_set(wchar_t (&result)[34], const wchar_t* name)
+void input_set(wchar_t (&result)[33], const wchar_t* name)
 {
 	std::wcout << L"Enter set " << name << L": ";
-	std::wcin.getline(result, 34);
+	std::wcin.getline(result, 33);
 }
 
-void print_set(const wchar_t (&set)[34])
+void print_set(const wchar_t (&set)[33])
 {
 	for (int i = 0; set[i] != '\0'; i++) {
 		std::wcout << set[i];
 	}
 	std::wcout << '\n';
 }
-
-void print_list(const List* head)
+void print_set(const List* set)
 {
-	for (const List* p = head; p != nullptr; p = p->next) {
-        std::wcout << p->el;
-    }
-    std::wcout << L'\n';
+	for (; set != nullptr; set = set->next) {
+		std::wcout << set->el;
+	}
+	std::wcout << '\n';
+}
+void print_set(uint64_t set)
+{
+	for (int i = 0; i < 33; i++) {
+		if ((set >> i) & 1) {
+			std::wcout << alphabet[i];
+		}
+	}
+	std::wcout << '\n';
 }
 
-// Function to generate a random set of characters
-void set_fill_random(wchar_t (&result)[34])
+void set_fill_random(wchar_t (&result)[33])
 {
-	const wchar_t alphabet[] = L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 	int i = 0;
 	for (const wchar_t& letter : alphabet) {
 		if (rand() % 2 == 1) {
@@ -131,7 +140,33 @@ void set_fill_random(wchar_t (&result)[34])
 	result[i] = '\0';
 }
 
-bool contains(const wchar_t (&arr)[34], wchar_t elem)
+List* arr_to_list(const wchar_t (&arr)[33])
+{
+	List* head = nullptr;
+	List* tail = nullptr;
+
+	for (int i = 0; arr[i] != L'\0'; i++) {
+		List* node = new List(arr[i]);
+		if (!head) {
+			head = node;
+			tail = node;
+		} else {
+			tail->next = node;
+			tail = node;
+		}
+	}
+	return head;
+}
+uint64_t arr_to_qword(const wchar_t (&arr)[33])
+{
+	uint64_t out = 0;
+	for (size_t i = 0; arr[i] != '\0'; i++) {
+		out = out | (1 << (arr[i] - alphabet[0]));
+	}
+	return out;
+}
+
+bool contains(const wchar_t (&arr)[33], wchar_t elem)
 {
 	for (int i = 0; arr[i] != '\0'; i++) {
 		if (arr[i] == elem) {
@@ -150,55 +185,84 @@ bool contains(const List* l, wchar_t elem)
 	return false;
 }
 
-// Function arr to list
-List* arr_to_list(const wchar_t (&arr)[34])
-{
-	List* head = nullptr;
-    List* tail = nullptr;
-
-    for (int i = 0; arr[i] != L'\0'; i++) {
-        List* node = new List(arr[i]);
-        if (!head) {
-            head = node;
-            tail = node;
-        } else {
-            tail->next = node;
-            tail = node;
-        }
-    }
-    return head;
-}
-
-void calc_set_arr(const wchar_t (&A)[34], const wchar_t (&B)[34],
-		const wchar_t (&C)[34], const wchar_t (&D)[34],
-		wchar_t (&out)[34])
+void calc_set(const wchar_t (&A)[33], const wchar_t (&B)[33],
+		const wchar_t (&C)[33], const wchar_t (&D)[33],
+		wchar_t (&out)[33])
 {
 	int i = 0;
-	for (int j = 0; A[j] != '\0'; j++) {
-		if (contains(B, A[j]) && contains(C, A[j]) && !contains(D, A[j])) {
-			out[i++] = A[j];
+	for (int j = 0; C[j] != '\0'; j++) {
+		if ((contains(A, C[j]) || contains(B, C[j])) && !contains(D, C[j])) {
+			out[i++] = C[j];
 		}
 	}
 	out[i] = '\0';
 }
-
-List* calc_set_list(const List* A, const List* B, const List* C, const List* D) 
+List* calc_set(const List* A, const List* B, const List* C, const List* D) 
 {
-    List* head = nullptr;
-    List* tail = nullptr;
+	List* head = nullptr;
+	List* tail = nullptr;
 
-    for (const List* p = A; p != nullptr; p = p->next) {
-        wchar_t ch = p->el;
-        if (contains(B, ch) && contains(C, ch) && !contains(D, ch)) {
-            List* node = new List(ch);
-            if (!head) {
-                head = node;
-                tail = node;
-            } else {
-                tail->next = node;
-                tail = node;
-            }
-        }
-    }
-    return head;
+	for (const List* p = C; p != nullptr; p = p->next) {
+		wchar_t ch = p->el;
+		if ((contains(A, ch) || contains(B, ch)) && !contains(D, ch)) {
+			List* node = new List(ch);
+			if (!head) {
+				head = node;
+				tail = node;
+			} else {
+				tail->next = node;
+				tail = node;
+			}
+		}
+	}
+	return head;
+}
+uint64_t calc_set(uint64_t A, uint64_t B, uint64_t C, uint64_t D)
+{
+	return (A | B) & C & (~D);
+}
+
+void benchmark(const wchar_t (&A)[33], const wchar_t (&B)[33],
+		const wchar_t (&C)[33], const wchar_t (&D)[33])
+{
+	auto t_start = std::chrono::high_resolution_clock::now();
+
+	wchar_t E[33];
+	calc_set(A, B, C, D, E);
+
+	auto t_end = std::chrono::high_resolution_clock::now();
+	double time_elapsed = std::chrono::duration<double, std::milli>(
+			t_end - t_start).count();
+	std::wcout << L"Time elapsed (array): " << time_elapsed << '\n';
+
+	std::wcout << L"Resulting set E: ";
+	print_set(E);
+}
+void benchmark(const List* A, const List* B, const List* C, const List* D)
+{
+	auto t_start = std::chrono::high_resolution_clock::now();
+
+	List* E = calc_set(A, B, C, D);
+
+	auto t_end = std::chrono::high_resolution_clock::now();
+	double time_elapsed = std::chrono::duration<double, std::milli>(
+			t_end - t_start).count();
+	std::wcout << L"Time elapsed (list): " << time_elapsed << '\n';
+
+	std::wcout << L"Resulting set E: ";
+	print_set(E);
+}
+void benchmark(uint64_t A, uint64_t B, uint64_t C, uint64_t D)
+{
+	auto t_start = std::chrono::high_resolution_clock::now();
+
+	uint64_t E = calc_set(A, B, C, D);
+
+	auto t_end = std::chrono::high_resolution_clock::now();
+	double time_elapsed = std::chrono::duration<double, std::milli>(
+			t_end - t_start).count();
+	std::wcout << L"Time elapsed (quad word): " << time_elapsed << '\n';
+
+	std::wcout << L"Resulting set E: ";
+	print_set(E);
 }
